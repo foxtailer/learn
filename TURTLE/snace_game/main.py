070140c1387d0d34ac3b7fd2110d9ce3127b4a67
Foxtailer <yaroslavdd1992@gmeil.com>
2023-01-11 01:26:38 +0200

    control snake

--- TURTLE/snace_game/main.py
+++ TURTLE/snace_game/main.py
@@ -8,12 +8,19 @@ screen.bgcolor("black")
 screen.title("My snake Game")
 screen.tracer(0)
 
-x = Snake()
-while True:
-    x.move()
-    screen.update()
-    time.sleep(0.2)
+snake = Snake()
 
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+while True:
+    screen.update()
+    time.sleep(0.1)
+
+    snake.move()
 
 
 screen.exitonclick()
--- TURTLE/snace_game/snake.py
+++ TURTLE/snace_game/snake.py
@@ -1,22 +1,41 @@
 from turtle import Turtle
 
 class Snake:
+    _tern = ""
     def __init__(self) -> None:
         self.bones = 3
         self.body = []
-        self._x = 0
+        self.create_snake()
+    
+    def create_snake(self):
+        _x = 0
         for _ in range(self.bones):
             bone = Turtle(shape="square")
             bone.penup()
             bone.color("white")
-            bone.setx(self._x)
+            bone.setx(_x)
             self.body.append(bone)
-            self._x += 20
-            #bone.shapesize(stretch_len = 20, stretch_wid = 20)
+            _x += 20
     
+    def up(self):
+        if self.body[0].heading() != 270:
+            self.body[0].setheading(90)
+
+    def down(self):
+        if self.body[0].heading() != 90:
+            self.body[0].setheading(270)
+
+    def left(self):
+        if self.body[0].heading() != 0:
+            self.body[0].setheading(180)
+
+    def right(self):
+        if self.body[0].heading() != 180:
+            self.body[0].setheading(0)
+
     def move(self):
         for index in range(len(self.body) - 1, 0, -1):
             self.body[index].goto(self.body[index - 1].pos())
         self.body[0].forward(20)
-        self.body[0].left(90)
+        #self.body[0].left(90)
 
