bac4028e3da99521a9e97eaa7fcb07d5a4fc73e3
yar <yaroslavdd1992@gmail.com>
2023-01-12 17:10:55 +0200

    score table done

--- TURTLE/snace_game/main.py
+++ TURTLE/snace_game/main.py
@@ -1,8 +1,10 @@
 from turtle import Screen
 from snake import Snake
 from food import Food
+from scoreboard import ScoreBoard
 import time
 
+is_on = True
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
@@ -11,6 +13,7 @@ screen.tracer(0)
 
 snake = Snake()
 food = Food()
+scoreboard = ScoreBoard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -18,10 +21,19 @@ screen.onkey(snake.down, "Down")
 screen.onkey(snake.left, "Left")
 screen.onkey(snake.right, "Right")
 
-while True:
+while is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
+
+    if snake.body[0].distance(food.pos()) < 15:
+        food.refresh()
+        scoreboard.resume()
+    
+    if snake.body[0].xcor() < - 290 or snake.body[0].xcor() > 290 or snake.body[0].ycor() > 290 or snake.body[0].ycor() < -290:
+        scoreboard.gameover()
+        is_on = False
+
 
 
 screen.exitonclick()
