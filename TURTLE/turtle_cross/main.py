ffd30058f7abbb4b38a185f1ed86cb19c2a20ab7
Foxtailer <yaroslavdd1992@gmeil.com>
2023-01-14 11:04:52 +0200

    Start day 23

---
+++ TURTLE/turtle_cross/main.py
@@ -0,0 +1,12 @@
+from turtle import Turtle, Screen
+import random, time
+
+SCREEN_X = 600
+SCREEN_Y = 600
+
+screen = Screen()
+turtle = Turtle()
+
+screen.exitonclick()
+screen.setup(SCREEN_X, SCREEN_Y)
+screen.title("Cross Game")
