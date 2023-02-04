dfd71243d325cfa794a8e894e060f1683e2a2626
Foxtailer <yaroslavdd@gmail.com>
2023-02-04 07:48:11 +0200

    work on pomodoro timer

--- tkinter/pomodoro/main.py
+++ tkinter/pomodoro/main.py
@@ -9,30 +9,47 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 
+def tryy():
+    pass
+
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
-
+def start_timer(minutes):
+    countdown(minutes)
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
-
+def countdown(minutes):
+    a = minutes // 10
+    b = minutes % 10
+    c = 5
+    d = 9
+    canvas.itemconfig(timer_text, text = f"{a}{b} : {c}{d}")
 # ---------------------------- UI SETUP ------------------------------- #
 window = tk.Tk()
 window.title("Pomodoro")
-window.minsize(500, 400)
+window.minsize(300, 300)
 window.config(padx=10, pady=10, bg=YELLOW)
 
-timer_lbl = tk.Label(text="Miles", font=("Arial", 14, "bold"))
-timer_lbl.grid(column=2, row=0)
+
+timer_lbl = tk.Label(text="Time", font=("Arial", 14, "bold"), fg=GREEN)
+timer_lbl.grid(column=1, row=0)
+
 
 bg_img = tk.PhotoImage(file=r"E:\git\learn\tkinter\pomodoro\tomato.png")
 canvas = tk.Canvas(width=210, height=224, bg=YELLOW, highlightthickness=0)
 canvas.create_image(105, 112, image=bg_img)
-canvas.create_text(110, 130, text="00:00", fill="grey", font=("Arial", 24, "bold"))
-canvas.grid()
+timer_text = canvas.create_text(110, 130, text="00:00", fill="grey", font=("Arial", 24, "bold"))
+canvas.grid(column=1, row=1)
 
-start_btn = tk.Button(text="Start", command=calculate)
-start_btn.grid(column=1, row=0)
-reset_btn = tk.Button(text="Reset", command=calculate)
-reset_btn.grid(column=1, row=2)
+
+start_btn = tk.Button(text="Start", command=start_timer(15), highlightthickness=0)
+start_btn.grid(column=0, row=2)
+
+reset_btn = tk.Button(text="Reset", command=tryy, highlightthickness=0)
+reset_btn.grid(column=2, row=2)
+
+
+pomodoros_lbl = tk.Label(text="*", font=("Arial", 14, "bold"))
+pomodoros_lbl.grid(column=1,row=3)
 
 window.mainloop()
