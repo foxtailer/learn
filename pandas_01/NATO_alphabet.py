de88da67cc639350224f0e5c2c25d65049bdeac5
Foxtailer <yaroslavdd@gmail.com>
2023-03-17 06:39:31 +0200

    _

--- pandas_01/NATO_alphabet.py
+++ pandas_01/NATO_alphabet.py
@@ -1,13 +1,33 @@
 import pandas as pd
 
-data = pd.read_csv(r"C:\Users\User\Desktop\glovo\git\learn\pandas_01\nato_phonetic_alphabet.csv")
+data = pd.read_csv(r"E:\YD\git\learn\pandas_01\nato_phonetic_alphabet.csv")
 NATO_ALPHABET = {row.letter : row.code for (index, row) in data.iterrows()}
 
-while True:
+# while True:
+#     word = input("Word for translate: ")
+#     # result = []
+#     # for i in word:
+#     #     if i.upper() in NATO_ALPHABET:
+#     #         result.append(NATO_ALPHABET[i.upper()])
+#     try:
+#         result = [NATO_ALPHABET[letter.upper()] for letter in word]
+#         print(result, "\n")
+#     except KeyError:
+#         print("Something wrong!")
+
+
+def nato_translator():
     word = input("Word for translate: ")
-    # result = []
-    # for i in word:
-    #     if i.upper() in NATO_ALPHABET:
-    #         result.append(NATO_ALPHABET[i.upper()])
-    result = [NATO_ALPHABET[letter.upper()] for letter in word]
-    print(result)
+
+    try:
+        result = [NATO_ALPHABET[letter.upper()] for letter in word]
+    except KeyError:
+        print("Something wrong!", "\n")
+        nato_translator()
+    else:
+        print(result, "\n")
+    finally:
+        nato_translator()
+
+
+nato_translator()
