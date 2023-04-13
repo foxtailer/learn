87b6ffd899833eb18ae27cc042ede98fa71b1ec8
Foxtailer <yaroslavdd@gmail.com>
2023-04-13 22:00:09 +0300

    gg

---
+++ recursion/reduce.py
@@ -0,0 +1,9 @@
+def add(a, b): return a + b;
+
+def reduce(fn, *terms, initial=0):
+    if terms:
+        return fn(terms[0], reduce(fn, *terms[1:], initial=0))
+    else:
+        return initial
+
+print(reduce(add, 1,2,3,4))
