fc6eb72a99d3a2d8cbbf3a6a47c68d17542e47e1
foxtailer <73554338+foxtailer@users.noreply.github.com>
2023-05-31 14:05:12 +0300

    Update 02.py

--- 100_py_question/02.py
+++ 100_py_question/02.py
@@ -5,9 +5,19 @@
 # Then, the output should be:
 # 40320
 
+#01
 x = int(input("Factorial of: "))
 result = 1
 for i in range(2, x+1):
     result *= i
 
-print(result)
+print("01: ",result)
+
+#02
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+print(factorial(x))
