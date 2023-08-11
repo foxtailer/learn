9423f67b4e16ba436bc00cdbfd7a3ad809558c23
Foxtailer <yaroslavdd1992@gmeil.com>
2023-08-11 07:26:51 +0300

    pygame 4

---
+++ pygame/Kinsley_pg/4/4-2_1.py
@@ -0,0 +1,77 @@
+import pygame
+from pygame.locals import *
+from sys import exit
+
+pygame.init()
+
+screen = pygame.display.set_mode((640, 480), 0, 32)
+
+# Creates images with smooth gradients
+def create_scales(height):
+    red_scale_surface = pygame.surface.Surface((640, height))
+    green_scale_surface = pygame.surface.Surface((640, height))
+    blue_scale_surface = pygame.surface.Surface((640, height))
+    
+    for x in range(640):
+        c = int((x/639.) * 255.)
+        red = (c, 0, 0)
+        green = (0, c, 0)
+        blue = (0, 0, c)
+        line_rect = Rect(x, 0, 1, height)
+        pygame.draw.rect(red_scale_surface, red, line_rect)
+        pygame.draw.rect(green_scale_surface, green, line_rect)
+        pygame.draw.rect(blue_scale_surface, blue, line_rect)
+
+    return red_scale_surface, green_scale_surface, blue_scale_surface
+
+
+def darker_color(color, value=0.5):
+    red, green, blue = color
+    red = red*value
+    green = green*value
+    blue = blue*value
+    return red, green, blue
+
+
+def brighter_color(color, value=1.5):
+    red, green, blue = color
+    red = min(red*value, 255)
+    green = min(green*value, 255)
+    blue = min(blue*value, 255)
+    return red, green, blue
+
+
+red_scale, green_scale, blue_scale = create_scales(80)
+color = [127, 127, 127]
+
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            exit()
+    
+    screen.fill((0, 0, 0))
+
+    # Draw the scales to the screen
+    screen.blit(red_scale, (0, 00))
+    screen.blit(green_scale, (0, 80))
+    screen.blit(blue_scale, (0, 160))
+    x, y = pygame.mouse.get_pos()
+
+    # If the mouse was pressed on one of the sliders, adjust the color component
+    if pygame.mouse.get_pressed()[0]:
+        for component in range(3):
+            if y > component*80 and y < (component+1)*80:
+                color[component] = int((x/639.)*255.)
+                pygame.display.set_caption("PyGame Color Test - "+str(tuple(color)))
+
+    # Draw a circle for each slider to represent the currentsetting
+    for component in range(3):
+        pos = ( int((color[component]/255.)*639), component*80+40 )
+        pygame.draw.circle(screen, (255, 255, 255), pos, 20)
+
+    pygame.draw.rect(screen, tuple(color), (170, 240, 300, 240))
+    pygame.draw.rect(screen, darker_color(color), (0, 240, 170, 240))
+    pygame.draw.rect(screen, brighter_color(color), (470, 240, 170, 240))
+
+    pygame.display.update()
--- test2.py
+++ test2.py
@@ -1,29 +1,7 @@
-# aiter
+i = 0
+for _ in range(256):
+    for _ in range(256):
+        for _ in range(256):
+            i += 1
 
-import asyncio
-
-class AsyncIterator:
-    def __init__(self, start, stop):
-        self.start = start
-        self.stop = stop
-
-    def __aiter__(self):
-        self.cur = self.start
-        return self
-    
-    async def __anext__(self):
-        await asyncio.sleep(1)
-        if self.cur >= self.stop:
-            raise StopAsyncIteration
-        
-        self.cur += 1 
-        return self.cur -1
-    
-async def example():
-    custom_obj = AsyncIterator(1, 10)
-    obj_iter =  aiter(custom_obj)
-    print(obj_iter)
-    async for num in obj_iter:
-        print(num)
-
-asyncio.run(example())
+print(i)
