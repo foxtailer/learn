fa7d6a73bdea1d686d460222640e00dd259288b1
Your Name <you@example.com>
2024-03-24 11:19:54 +0200

    gg

--- projects/web/dior/js/main.js
+++ projects/web/dior/js/main.js
@@ -3,7 +3,22 @@ const swiper = new Swiper('.swiper', {
   //direction: 'vertical',
   loop: true,
 
+  // autoplay: {
+  //   delay: 3000,
+  //   disableOnINteraction: false,
+  // },
+
   // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+var swiper2 = new Swiper(".about__slider", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  freeMode: true,
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
