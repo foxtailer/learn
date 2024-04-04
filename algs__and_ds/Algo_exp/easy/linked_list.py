d12f7d3cf916c411bb6e3182ac891f4a579138ae
Foxtailer <yaroslavdd1992@gmail.com>
2024-04-04 06:24:55 -0700

    gg

---
+++ algs__and_ds/Algo_exp/easy/linked_list.py
@@ -0,0 +1,28 @@
+class DoublyLinkedList:
+  def __init__(self) -> None:
+    self.head = None
+    self.tail = None
+
+  def setHead(self, node):
+    pass
+
+  def setTail(self, node):
+    pass
+  
+  def insertBefore(self, node, nodeToInsert):
+    pass
+  
+  def insertAfter(self, node, nodeToInsert):
+    pass
+
+  def insertAtPosition(self, position, nodeToInsert):
+    pass
+  
+  def romoveNodesWithValue(self, value):
+    pass
+  
+  def remove(self, node):
+    pass
+  
+  def containsNodeWithValue(self, value):
+    pass
