613bd92b779ea27030bf5012b09b456528cba552
Your Name <you@example.com>
2024-04-13 13:24:20 +0300

    gg

--- _tests/_test.py
+++ _tests/_test.py
@@ -1,10 +1,3 @@
-def tower_builder(n_floors):
-    line_width = int(n_floors/2 * (1 + (1 + (n_floors-1)*2)))
-    result = []
-    floor = "*"
-    
-    for _ in range(n_floors):
-        result.append(floor.center(line_width, " "))
-        floor += "*"*2
-    return result
-print(tower_builder(3))
+
+
+print(chr(ord("b".encode('utf-8'))))
