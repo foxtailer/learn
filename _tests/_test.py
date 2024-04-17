5bc729705dc0cc6ee627ed4180d0be850902fcd6
Your Name <you@example.com>
2024-04-17 20:24:23 +0300

    gg

--- _tests/_test.py
+++ _tests/_test.py
@@ -1,3 +1,12 @@
+def scramble(s1, s2):
+    for i in s2:
+        if i in s1:
+            s1.replace(i,"",1)
+        else:
+            return False
+    return True
 
+s1 = "zggiskkvqquxui"
+s2 = "qkzqzk"
 
-print(chr(ord("b".encode('utf-8'))))
+print(scramble(s1, s2))
