56b3618f9c56c526ae4e7f0367db114917396f0b
Your Name <you@example.com>
2024-04-19 20:54:52 +0300

    gg

--- _tests/_test.py
+++ _tests/_test.py
@@ -1,12 +1,16 @@
 def scramble(s1, s2):
-    for i in s2:
-        if i in s1:
-            s1.replace(i,"",1)
-        else:
-            return False
-    return True
+    s1_dict = {}
+    for i in s1:
+        try:
+            s1_dict[i] += 1
+        except:
+            s1_dict[i] = 1
 
-s1 = "zggiskkvqquxui"
-s2 = "qkzqzk"
-
-print(scramble(s1, s2))
+s1 = "aafngekke"
+s1_dict = {}
+for i in s1:
+    try:
+        s1_dict[i] += 1
+    except:
+        s1_dict[i] = 1
+print(s1_dict)
