1cfa563b589c60d1b3485285fe6fb1d1aba7e070
Your Name <you@example.com>
2024-05-20 14:31:55 +0300

    goods load from content

--- projects/Django/store1/main/views.py
+++ projects/Django/store1/main/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
-
 # Create your views here.
 
 def index(request):
-  context = {'title':'Home', 'content':'Бла бла'}
+  context = {'title':'Home', 'content':'HOME - главная.'}
   return render(request, 'main/index.html', context)
 
 def about(request):
-  return HttpResponse("About page")
+  context = {'title':'Home', 'content':'HOME - О нас.'}
+  return render(request, 'main/about.html', context)
