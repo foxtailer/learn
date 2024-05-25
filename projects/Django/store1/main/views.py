eda5941fd181f3d4cefb06bb9aec003db033968c
Your Name <you@example.com>
2024-05-25 12:46:47 +0300

    learn

--- projects/Django/store1/main/views.py
+++ projects/Django/store1/main/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
+from goods.models import Categories
 
 # Create your views here.
 
 def index(request):
-  context = {'title':'Home', 'content':'HOME - главная.'}
+  categories = Categories.objects.all()
+
+  context = {'title':'Home',
+             'content':'HOME - главная.',
+              'categories': categories
+  }
+  
   return render(request, 'main/index.html', context)
 
 def about(request):
