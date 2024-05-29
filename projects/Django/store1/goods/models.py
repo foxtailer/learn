e7658237a5529937aa5ccec950b4ba1c6f206959
Your Name <you@example.com>
2024-05-29 13:30:35 +0300

    'store1'

--- projects/Django/store1/goods/models.py
+++ projects/Django/store1/goods/models.py
@@ -20,7 +20,7 @@ class Products(models.Model):
   description = models.TextField(blank=True, null=True, verbose_name='Описание')
   image = models.ImageField(upload_to='goods_image', blank=True, null=True, verbose_name='Изображение')
   price = models.DecimalField(default=0.00, max_digits=7, decimal_places=2, verbose_name='Цена')
-  discount = models.DecimalField(default=0.00, max_digits=7, decimal_places=2, verbose_name='Скидка в %')
+  discount = models.DecimalField(default=0.00, max_digits=4, decimal_places=2, verbose_name='Скидка в %')
   quantity = models.PositiveIntegerField(default=0, verbose_name='Количество')
   category = models.ForeignKey(to=Categories, on_delete=models.PROTECT, verbose_name='Категория')
 
@@ -28,7 +28,17 @@ class Products(models.Model):
     db_table = 'product'
     verbose_name = 'Продукт'
     verbose_name_plural = 'Продукты'
+    ordering = ('id',)
 
   def __str__(self):
     return self.name
+  
+  def display_id(self):
+    return f'{self.id:05}'
+  
+  def sell_price(self):
+    if self.discount:
+      return round(self.price - (self.price/100)*self.discount, 2)
+    return self.price
+
 
--- projects/Django/store1/goods/urls.py
+++ projects/Django/store1/goods/urls.py
@@ -21,6 +21,6 @@ from . import views
 app_name = 'goods'
 
 urlpatterns = [
-    path('', views.catalog, name='index'),
-    path('product/', views.product, name='product'),
+    path('<slug:slug>/', views.catalog, name='index'),
+    path('product/<slug:slug>/', views.product, name='product'),
 ]
