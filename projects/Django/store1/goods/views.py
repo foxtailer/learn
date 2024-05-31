4c33b5c1d48737a4b359bc1cba9479052420b480
Your Name <you@example.com>
2024-05-31 09:54:58 +0300

    pagination v1

--- projects/Django/store1/goods/views.py
+++ projects/Django/store1/goods/views.py
@@ -3,28 +3,28 @@ from django.core.paginator import Paginator
 from goods.models import Products
 # Create your views here.
 
-def catalog(request, slug):
+def catalog(request, category_slug, page=1):
 
-  if slug == "all":
+  if category_slug == "all":
     goods = Products.objects.all()
   else:
-    goods = get_list_or_404(Products.objects.filter(category__slug=slug))
+    goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
 
   p = Paginator(goods, 3)
-  current_page = p.page(1)
+  current_page = p.page(page)
 
   context = {
     'title': 'Home - Каталог',
-    'goods': current_page
+    'goods': current_page,
+    'slug_url': category_slug
   }
   return render(request, 'goods/catalog.html', context)
 
-def product(request, slug):
+def product(request, product_slug):
 
-  product = Products.objects.get(slug=slug)
+  product = Products.objects.get(slug=product_slug)
 
   context = {
     'product': product
   }
-
   return render(request, 'goods/product.html', context)
