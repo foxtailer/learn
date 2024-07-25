54e641ed291838bdefc1b4c08d72187214efdf78
Your Name <you@example.com>
2024-07-25 13:56:46 +0300

    store2 text search ++

--- projects/Django/store2/app/settings.py
+++ projects/Django/store2/app/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.postgres',
     'debug_toolbar',
 
     'main',
@@ -60,7 +61,7 @@ ROOT_URLCONF = 'app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- projects/Django/store2/goods/utils.py
+++ projects/Django/store2/goods/utils.py
@@ -1,5 +1,5 @@
-from django.db.models import Q
 from goods.models import Product
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank, SearchHeadline
 
 
 def q_search(query):
@@ -7,12 +7,17 @@ def q_search(query):
   if query.isdigit() and len(query) <= 5:
     return Product.objects.filter(id=int(query))
   
-  keywords = [word for word in query.split() if len(word) > 2]
+  vector = SearchVector('name', 'description')
+  query = SearchQuery(query)
 
-  q_object = Q()
+  result = Product.objects.annotate(rank=SearchRank(vector, query)).filter(rank__gt=0).order_by('-rank')
 
-  for token in keywords:
-    q_object |= Q(description__icontains=token)
-    q_object |= Q(name__icontains=token)
-
-  return Product.objects.filter(q_object)
+  result = result.annotate(
+    headline=SearchHeadline(
+      'name',
+      query,
+      start_sel='<span style="background-color: yellow;">',
+      stop_sel='</span>',
+    )
+  )
+  return result
