c551bed3cad29d078c0ba9a17559fd409a1746eb
Your Name <you@example.com>
2024-09-01 10:16:17 -0400

    learn

--- asyncio/asyncio_bot_learn/set_webhook.py
+++ asyncio/asyncio_bot_learn/set_webhook.py
@@ -1,10 +1,10 @@
 import requests
 
 # Replace with your bot's token
-BOT_TOKEN = "6917432152:AAE_D5ua9YN4xWCr30ySC-rOvdP0hEpNMcI"
+BOT_TOKEN = ""
 
 # Replace with your webhook URL
-WEBHOOK_URL = "https://foxtailer.pythonanywhere.com/webhook"
+WEBHOOK_URL = ""
 
 # The URL for setting the webhook
 set_webhook_url = f"https://api.telegram.org/bot{BOT_TOKEN}/setWebhook"
--- asyncio/asyncio_bot_learn/webhook_serv.py
+++ asyncio/asyncio_bot_learn/webhook_serv.py
@@ -2,10 +2,17 @@ from aiogram import Bot, Dispatcher
 from aiogram.webhook.aiohttp_server import SimpleRequestHandler
 from aiohttp import web
 
+
 # Create instances of Bot and Dispatcher
-API_TOKEN = 'YOUR_BOT_API_TOKEN'
+API_TOKEN = ''
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher()
+
+
+@dp.message()
+async def echo_message(msg):
+    await bot.send_message(msg.from_user.id, msg.text)
+
 
 # Create a SimpleRequestHandler instance
 webhook_handler = SimpleRequestHandler(
@@ -15,11 +22,7 @@ webhook_handler = SimpleRequestHandler(
     secret_token=None           # Optional, used for validating incoming requests
 )
 
-@dp.message()
-async def echo_message(msg):
-    await bot.send_message(msg.from_user.id, msg.text)
 
-webhook_handler.handle
 # Create and set up the aiohttp web application
 app = web.Application()
 webhook_handler.register(app, path='/webhook')
@@ -27,4 +30,4 @@ webhook_handler.register(app, path='/webhook')
 
 # Run the aiohttp application
 if __name__ == '__main__':
-    web.run_app(app, port=8443)
+    web.run_app(app, port=88)
