80db3bdeccd3ba9398a0f80148f08086f1aabf73
Foxtailer <yaroslavdd1992@gmail.com>
2025-03-14 07:11:31 -0600

    bool data type is a subclass of the int data type.

--- _tests/_test.py
+++ _tests/_test.py
@@ -1,6 +1,6 @@
 ############# Decorators ##########
 import sys
-folder = '/home/zoy/vscode/learn/courses/Python Tutorial'
+folder = '/home/zoy/git/learn/courses/Python Tutorial'
 if folder not in sys.path:
     sys.path.append(folder)
 
@@ -8,13 +8,7 @@ from decorators_pycon import CountCalls
 ###################################
 
 
-@CountCalls
-def fibo(number):
-    if number < 2:
-        return 1
-    
-    return fibo(number -1) + fibo(number - 2)
-
-
-print(fibo(7))
-print(fibo.num_calls)
+letters = ['z', 'A', 'a', 'Z']
+letters.sort(key=str.lower)
+#letters.sort(key=lambda x:x.lower())
+print(letters)
