4bf78a40aee55207519d29f0fb4bfdca89ed2476
Foxtailer <yaroslavdd1992@gmail.com>
2025-04-10 12:35:50 -0600

    bootstrap yt finish

--- courses/Algo_exp/ds/doubly_linked_list.py
+++ courses/Algo_exp/ds/doubly_linked_list.py
@@ -1,12 +1,23 @@
 import doctest
 
-doctest.testmod()
 
 class Node:
+    """
+    >>> y = Node(1)
+    >>> x = Node(1)
+    >>> x == y
+    True
+    >>> x is y
+    False
+    """
     def __init__(self, data):
         self.data = data
         self.next = None
         self.prev = None
+
+    def __eq__(self, data):
+        return self.data == data.data
+
 
 class DoublyLinkedList:
     def __init__(self):
@@ -14,7 +25,7 @@ class DoublyLinkedList:
         self.tail = None  # Keep track of the last node
 
     # O(1)
-    def insert_at_beginning(self, data):
+    def insert_at_beginning(self, data:Node):
         """
         >>> dll = DoublyLinkedList()
         >>> dll.display()
@@ -22,11 +33,17 @@ class DoublyLinkedList:
         >>> dll.insert_at_beginning(1)
         >>> dll.display()
         [1]
+        >>> dll.head is dll.tail
+        True
         >>> dll.insert_at_end(2)
         >>> dll.display()
         [1<->2]
+        >>> dll.insert_at_beginning(1)
+        >>> dll.display()
+        [1<->1<->2]
         """
         new_node = Node(data)
+
         if not self.head:
             self.head = self.tail = new_node
             self.head.next = self.head  # Circular link
@@ -40,7 +57,21 @@ class DoublyLinkedList:
 
     # O(1)
     def insert_at_end(self, data):
+        """
+        >>> dll = DoublyLinkedList()
+        >>> dll.display()
+        []
+        >>> dll.insert_at_end(1)
+        >>> dll.display()
+        [1]
+        >>> dll.head == dll.tail
+        True
+        >>> dll.insert_at_end(2)
+        >>> dll.display()
+        [1<->2]
+        """
         new_node = Node(data)
+
         if not self.head:
             self.head = self.tail = new_node
             self.head.next = self.head  # Circular link
@@ -54,6 +85,22 @@ class DoublyLinkedList:
 
     # O(1)
     def pop_head(self):
+        """
+        >>> dll = DoublyLinkedList()
+        >>> dll.insert_at_end(1)
+        >>> dll.insert_at_end(2)
+        >>> dll.insert_at_end(3)
+        >>> dll.pop_head()
+        1
+        >>> dll.display()
+        [2<->3]
+        >>> dll = DoublyLinkedList()
+        >>> dll.insert_at_end(1)
+        >>> dll.pop_head()
+        1
+        >>> dll.display()
+        []
+        """
         if not self.head:
             return None  # Empty list
         data = self.head.data
@@ -67,6 +114,16 @@ class DoublyLinkedList:
 
     # O(1)
     def pop_tail(self):
+        """
+        >>> dll = DoublyLinkedList()
+        >>> dll.insert_at_end(1)
+        >>> dll.insert_at_end(2)
+        >>> dll.insert_at_end(3)
+        >>> dll.pop_tail()
+        3
+        >>> dll.display()
+        [1<->2]
+        """
         if not self.head:
             return None  # Empty list
         data = self.tail.data
@@ -87,6 +144,34 @@ class DoublyLinkedList:
         while True:
             result.append(str(temp.data))
             temp = temp.next
-            if temp == self.head:
+            if temp is self.head:
                 break
         print(f'[{"<->".join(result)}]')
+
+    def insert_before(self, node, data:Node):
+        ...
+
+    def insert_after(self, node, data:Node):
+        ...
+
+    def insert_at_position(self, index, data:Node):
+        ...
+
+    def remove_node_with_value(self, data):
+        ...
+
+    def remove(self, data:Node):
+        if self.head is self.tail:
+            if self.head is data:
+                self.head = self.tail = None
+            else:
+                raise ValueError('Node not in list')
+            
+        if self.head is data:
+            self.tail.next = self.head = self.head.next
+        if self.tail is data:
+            self.head.prev = self.tail = self.tail.prev
+
+
+    
+doctest.testmod()
