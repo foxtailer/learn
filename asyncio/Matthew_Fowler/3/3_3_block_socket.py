c1d6287267b53b150e89aba4e6480ffa5c58f5b8
foxtailer <less.is.more@mailfence.com>
2025-04-14 11:08:58 -0600

    asyncio

---
+++ asyncio/Matthew_Fowler/3/3_3_block_socket.py
@@ -0,0 +1,51 @@
+import socket
+
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+server_address = ('127.0.0.1', 8000)
+server_socket.bind(server_address)
+server_socket.listen()
+connections = []
+
+
+try:
+    # while True:
+    #     connection, client_address = server_socket.accept()
+    #     print(f'Получен запрос на подключение от {client_address}!')
+    #     connections.append(connection)
+
+    #     for connection in connections:
+    #         buffer = b''
+
+    #         while buffer[-2:] != b'\r\n':
+    #             data = connection.recv(2)
+    #             if not data:
+    #                 break
+    #             else:
+    #                 print(f'Получены данные: {data}!')
+    #                 buffer = buffer + data
+                    
+    #         print(f"Все данные: {buffer}")
+    #         connection.send(buffer)  # BrokenPipeError
+    while True:
+        connection, client_address = server_socket.accept()
+        print(f'Получен запрос на подключение от {client_address}!')
+        buffer = b''
+        try:
+            while buffer[-2:] != b'\r\n':
+                data = connection.recv(2)
+                if not data:
+                    break
+                print(f'Получены данные: {data}!')
+                buffer += data
+            print(f"Все данные: {buffer}")
+            connection.send(buffer)
+        except BrokenPipeError:
+            print(f"Клиент {client_address} закрыл соединение до отправки ответа.")
+        finally:
+            connection.close()
+
+finally:
+    server_socket.close()
