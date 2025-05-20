6997359cb49653f44224eb0c88cbc1d2577ff9a5
foxtailer <less.is.more@mailfence.com>
2025-05-20 04:32:54 -0600

    model Item for lists

--- courses/books/TDD/02_func_tests.py
+++ courses/books/TDD/02_func_tests.py
@@ -13,6 +13,15 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def chec_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertIn(row_text, [row.text for row in rows])
+        # self.assertTrue(
+        #     any(row.text == '1: Buy peacock feathers' for row in rows),
+        #     f"New to-do item did not appear in table. Contents were:\n{table.text}"
+        # )
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
@@ -40,16 +49,19 @@ class NewVisitorTest(unittest.TestCase):
     	# "1: Buy peacock feathers" as an item in a to-do list table
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
+        self.chec_for_row_in_list_table("1: Buy peacock feathers")
         
-        table = self.browser.find_element(By.ID, 'id_list_table')
-        rows = table.find_element(By.TAG_NAME, 'tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows)
-        )
-
     	# There is still a text box inviting her to add another item. She
     	# enters "Use peacock feathers to make a fly" (Edith is very methodical)
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
     	# The page updates again, and now shows both items on her list
+        self.chec_for_row_in_list_table("1: Buy peacock feathers")
+        self.chec_for_row_in_list_table("2: Use peacock feathers to make a fly")
+
     	# Edith wonders whether the site will remember her list. Then she sees
     	# that the site has generated a unique URL for her -- there is some
     	# explanatory text to that effect.
--- courses/books/TDD/superlist/lists/views.py
+++ courses/books/TDD/superlist/lists/views.py
@@ -3,5 +3,12 @@ from django.http import HttpResponse
 
 
 def home_page(request):
+    if request.method == 'POST':
+        context = {
+            'new_item_text': request.POST.get('item_text', '')
+        }
+        return render(request, 'lists/home.html', context=context)
+        # return HttpResponse(request.POST['item_text'])
+
     return render(request, 'lists/home.html')
 
